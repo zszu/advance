0978e003cd68801015c1da972c4a74f7b4aa0821
zszu <2835152688@qq.com>
2021-03-05 15:47:54 +0800

    update

---
+++ backend/config/main-local.php
@@ -0,0 +1,24 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '8OACTYwPbTT44rhgq9-36JunEIap6rBm',
+        ],
+    ],
+];
+if (YII_DEBUG) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
